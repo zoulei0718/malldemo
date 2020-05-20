d052652cccf628d7bc89a3a1e7110f9d7f6c470d
邹蕾 <zoulei@ZOULEI-MacBook-Pro.local>
2020-05-20 22:58:22 +0800

    新增navbar

---
+++ src/components/common/navbar/NavBar.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="nav-bar">
+    <div class="left">
+      <slot name="left"></slot>
+    </div>
+    <div class="center">
+      <slot name="center"></slot>
+    </div>
+    <div class="right">
+      <slot name="right"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "navbar"
+};
+</script>
+
+<style>
+.nav-bar {
+  display: flex;
+  height: 49px;
+  line-height: 49px;
+  text-align: center;
+  box-shadow: 0 1px 3px 3px rgba(100, 100, 100, 0.5);
+  background-color: tan;
+}
+.left, .rigth {
+  flex: 1;
+  height: 49px;
+  background-color: silver;
+}
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,10 +1,17 @@
 <template>
-  <div>Home</div>
+  <div id="home">
+    <nav-bar><div slot="center">购物街</div></nav-bar>
+  </div>
 </template>
 
 <script>
+import NavBar from 'components/common/navbar/NavBar'
+
 export default {
-    name:"Home"
+    name:"Home",
+    components:{
+      NavBar
+    }
 }
 </script>
 
